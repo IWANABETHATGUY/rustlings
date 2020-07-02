f468e879cb5b05faba84ad5791dc1ad2eaa5fd8e
IWANABETHATGUY <974153916@qq.com>
2020-07-02 13:51:17 +0800

    feat: 🎸 function

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,14 +1,13 @@
 // functions3.rs
 // Make me compile! Execute `rustlings hint functions3` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
     call_me();
 }
 
-fn call_me(num: i32) {
-    for i in 0..num {
+fn call_me() {
+    for i in 0..3 {
         println!("Ring! Call number {}", i + 1);
     }
 }
