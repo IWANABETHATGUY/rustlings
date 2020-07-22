44bc9d5261db575f9c3d1c775990bde5cd8688b5
IWANABETHATGUY <974153916@qq.com>
2020-07-22 13:19:04 +0800

    feat: 🎸 conversion

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -2,7 +2,7 @@
 // Basically, this is the same as From. The main difference is that this should return a Result type
 // instead of the target type itself.
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.TryFrom.html
-use std::convert::{TryInto, TryFrom};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(Debug)]
 struct Color {
@@ -11,7 +11,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -26,6 +25,14 @@ struct Color {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        match tuple {
+            (a @ 0..=255, b @ 0..=255, c @ 0..=255) => Ok(Color {
+                red: a as u8,
+                green: b as u8,
+                blue: c as u8,
+            }),
+            _ => Err("".to_string()),
+        }
     }
 }
 
@@ -33,6 +40,14 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        match arr {
+            [a @ 0..=255, b @ 0..=255, c @ 0..=255] => Ok(Color {
+                red: a as u8,
+                green: b as u8,
+                blue: c as u8,
+            }),
+            _ => Err("".to_string()),
+        }
     }
 }
 
@@ -40,6 +55,14 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = String;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        match slice {
+            [a @ 0..=255, b @ 0..=255, c @ 0..=255] => Ok(Color {
+                red: *a as u8,
+                green: *b as u8,
+                blue: *c as u8,
+            }),
+            _ => Err("".to_string()),
+        }
     }
 }
 
